edf6dcd2962a16c825ea25d6d7bcc48ba9ae658f
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2021-09-24 17:06:43 +0100

    deploy to heroku

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const express = require('express');
 //bring in the DB connection
 const connectDB = require('./config/db.js');
 
+//path module
+const path = require('path');
+
 //initialise app variable with express
 const app = express();
 
@@ -15,14 +18,21 @@ connectDB();
 //initialise middleware
 app.use(express.json({ extended: false }));
 
-//single end point to test and send something to the browser
-app.get('/', (req, res) => res.send('API Running'));
-
 //define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/discussions', require('./routes/api/discussions'));
+
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //setup the environment variable for local host or deployed server
 const PORT = process.env.PORT || 5000;
